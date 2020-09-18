8060bc5789864e79fcf6d67a9bb987167b517444
Rémi de Juvigny <remi@hey.com>
2020-09-18 13:48:24 +0200

    Add eslint and new structure

--- frontend/src/utils/medias.js
+++
@@ -1,9 +0,0 @@
-export function getStrapiMedia(url) {
-  // Check if URL is a local path
-  if (url.startsWith('/')) {
-    // Prepend Strapi address
-    return `http://localhost:1337${url}`
-  }
-  // Otherwise return full URL
-  return url
-}
