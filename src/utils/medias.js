d360659421602230c606e62cdcc43f55586e7356
Rémi de Juvigny <remi@hey.com>
2020-09-23 16:17:30 +0200

    Fix hardcoded Strapi URL

--- src/utils/medias.js
+++ src/utils/medias.js
@@ -2,7 +2,9 @@ export function getStrapiMedia(url) {
   // Check if URL is a local path
   if (url.startsWith("/")) {
     // Prepend Strapi address
-    return `http://localhost:1337${url}`;
+    return `${
+      process.env.GRIDSOME_STRAPI_URL || "http://localhost:1337"
+    }${url}`;
   }
   // Otherwise return full URL
   return url;
